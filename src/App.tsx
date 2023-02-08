c9387c9a95df6a513d2bd7bdea6ec9e1cf549649
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-08 16:49:23 -0300

    feat: component header added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.scss'
+import { Header } from './components/Header'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <div className="section-header">
+        <Header />
+      </div>
     </div>
   )
 }
